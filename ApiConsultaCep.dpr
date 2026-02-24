fee1151024e2f78ebe47512cb6a15ff61a5998c9
Raphael comba franco <raphaelcomba@gmail.com>
2026-02-24 14:31:14 -0300

    fisrt commit

---
+++ ApiConsultaCep.dpr
@@ -0,0 +1,17 @@
+program ApiConsultaCep;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils;
+
+begin
+  try
+
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+end.
